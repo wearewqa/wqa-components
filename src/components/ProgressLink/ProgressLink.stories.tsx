a9089682b57121b958a13d5005c2664d5591b311
greenandlonely <jclarke0206@gmail.com>
2022-02-23 14:10:17 +0000

    Adds ProgressLink component

---
+++ src/components/ProgressLink/ProgressLink.stories.tsx
@@ -0,0 +1,21 @@
+// YourComponent.stories.ts | YourComponent.stories.tsx
+
+import React, { ComponentProps } from "react";
+
+import { Story, Meta } from "@storybook/react";
+
+import ProgressLink from "./index";
+
+//👇 This default export determines where your story goes in the story list
+export default {
+  title: "Components/Progress Link",
+  component: ProgressLink,
+} as Meta;
+
+//👇 We create a “template” of how args map to rendering
+const Template: Story<ComponentProps<typeof ProgressLink>> = (args) => <ProgressLink {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: "Read more",
+};
