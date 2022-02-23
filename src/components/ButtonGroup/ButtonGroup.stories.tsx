ee88c9df7e9ac96e86c9b8880aa6e28c9021770c
greenandlonely <jclarke0206@gmail.com>
2022-02-23 11:24:01 +0000

    Adds ButtonGroup component

---
+++ src/components/ButtonGroup/ButtonGroup.stories.tsx
@@ -0,0 +1,33 @@
+// YourComponent.stories.ts | YourComponent.stories.tsx
+
+import React, { ComponentProps } from "react";
+
+import { Story, Meta } from "@storybook/react";
+
+import ButtonGroup from "./index";
+import Button from "../Button";
+
+//👇 This default export determines where your story goes in the story list
+export default {
+  title: "Components/Button Group",
+  component: ButtonGroup,
+} as Meta;
+
+//👇 We create a “template” of how args map to rendering
+const Template: Story<ComponentProps<typeof ButtonGroup>> = (args) => <ButtonGroup {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  direction: "row",
+  align: "start",
+  children: (
+    <>
+      <Button color="secondary-gray" size="xlarge" groupItem>
+        Button 1
+      </Button>
+      <Button color="primary" size="xlarge" groupItem>
+        Button 2
+      </Button>
+    </>
+  ),
+};
