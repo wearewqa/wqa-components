7d68078cb7f3d4e14bbacf981f4372bd736e2849
greenandlonely <jclarke0206@gmail.com>
2022-02-23 12:44:26 +0000

    Adds Checklist & ChecklistItem components

---
+++ src/components/Checklist/Checklist.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import classnames from "classnames";
+import "./Checklist.scss";
+
+type ChecklistProps = {
+  color: "primary" | "success";
+  size?: "small" | "medium" | "large" | "xlarge";
+  contextClassName?: string; // check approach w JC
+  code?: boolean;
+  children: React.ReactNode;
+};
+
+function Checklist({ color, size, contextClassName, code, children }: ChecklistProps): JSX.Element {
+  const baseClassName = "checklist";
+  const colorClassName = color ? `${baseClassName}--${color}` : "";
+  const sizeClassName = size ? `${baseClassName}--${size}` : "";
+  const classNames = classnames(baseClassName, colorClassName, sizeClassName, contextClassName);
+  const html = (
+    <>
+      <ul className={`${classNames}`}>{children}</ul>
+    </>
+  );
+  const htmlString = ReactDOMServer.renderToStaticMarkup(html);
+  if (code) {
+    return <code>{`${htmlString}`}</code>;
+  }
+  return html;
+}
+
+export default Checklist;
---
+++ src/components/ChecklistItem/ChecklistItem.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import classnames from "classnames";
+import Icon from "../Icon/Icon";
+
+type ChecklistItemProps = {
+  color?: "primary" | "success";
+  code?: boolean;
+  children: React.ReactNode;
+};
+
+function ChecklistItem({ color, code, children }: ChecklistItemProps): JSX.Element {
+  const baseClassName = "checklist__item";
+  const colorClassName = color ? `${baseClassName}--${color}` : "";
+  const classNames = classnames(baseClassName, colorClassName);
+  const html = (
+    <>
+      <li className={`${classNames}`}>
+        <Icon name="check" className="checklist__icon" />
+        {children}
+      </li>
+    </>
+  );
+  const htmlString = ReactDOMServer.renderToStaticMarkup(html);
+  if (code) {
+    return <code>{`${htmlString}`}</code>;
+  }
+  return html;
+}
+
+export default ChecklistItem;
