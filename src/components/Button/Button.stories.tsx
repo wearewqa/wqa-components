db6ec91727da070da087101bac7d93f657e701e9
greenandlonely <jclarke0206@gmail.com>
2022-04-25 19:12:03 +0100

    Button stories file updated to remove other versions

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -16,26 +16,26 @@ export default {
 //👇 We create a “template” of how args map to rendering
 const Template: Story<ComponentProps<typeof Button>> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const Default = Template.bind({});
+Default.args = {
   theme: Themes.default,
   type: "primary",
-  size: "large",
+  size: "medium",
   children: "Button",
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  theme: Themes.default,
-  type: "secondary",
-  size: "large",
-  children: "Button",
-};
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   theme: Themes.default,
+//   type: "secondary",
+//   size: "large",
+//   children: "Button",
+// };
 
-export const Tertiary = Template.bind({});
-Tertiary.args = {
-  theme: Themes.default,
-  type: "tertiary",
-  size: "large",
-  children: "Button",
-};
+// export const Tertiary = Template.bind({});
+// Tertiary.args = {
+//   theme: Themes.default,
+//   type: "tertiary",
+//   size: "large",
+//   children: "Button",
+// };
